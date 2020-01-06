7c435778cf7315eba64f3978cb9f011c592af377
AndreaDiotallevi <andrea.diotallevi@outlook.com>
2020-01-06 15:20:18 +0000

    Refactor test

--- note-model-tests.js
+++ note-model-tests.js
@@ -1,13 +1,28 @@
-(function(exports) {
-  function returnTestNoteText() {
-    var noteText = new Note('My favourite language is JavaScript');
+// (function(exports) {
+//   function returnTestNoteText() {
+//     var noteText = new Note('My favourite language is JavaScript');
 
-    if (noteText.getText() !== 'My favourite language is JavaScript') {
-      throw new Error("The note text is not equal to 'My favourite language is JavaScript'");
-    } else {
-      console.log("it returns the test note text");
-    };
-  };
+//     if (noteText.getText() !== 'My favourite language is JavaScript') {
+//       throw new Error("The note text is not equal to 'My favourite language is JavaScript'");
+//     } else {
+//       console.log("it returns the test note text");
+//     };
+//   };
 
-  returnTestNoteText();
-})(this);
+//   returnTestNoteText();
+// })(this);
+
+var assert = {
+  isTrue: function(assertionToCheck) {
+    if (!assertionToCheck) {
+      throw new Error("Assertion failed: " + assertionToCheck + " is not truthy");
+    }
+  }
+}
+
+function testReturnNoteText() {
+  var noteText = new Note('My favourite language is JavaScript');
+  assert.isTrue(noteText.getText() === 'My favourite language is JavaScript');
+}
+
+testReturnNoteText();
